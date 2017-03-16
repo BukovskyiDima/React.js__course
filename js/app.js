1d0729abae75dad1bb1133cd2f39d1182eb6d4ae
Dima Bukovskyi <bukovsky.dima@gmail.com>
2017-03-16 14:20:13 +0200

    react course final version

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ var my_news = [
     }
 ];
 
+window.ee = new EventEmitter();
+
 var Article = React.createClass({
     propTypes: {
         data: React.PropTypes.shape({
@@ -89,44 +91,104 @@ var News = React.createClass({
     }
 });
 
-// --- добавили test input ---
-var TestInput = React.createClass({
-
+var Add = React.createClass({
     getInitialState: function() {
         return {
-            myValue: ''
+            agreeNotChecked: true,
+            authorIsEmpty: true,
+            textIsEmpty: true
         };
     },
-
-    onChangeHandler: function(e) {
-        this.setState({myValue: e.target.value})
+    componentDidMount: function() {
+        ReactDOM.findDOMNode(this.refs.author).focus();
     },
+    onBtnClickHandler: function(e) {
+        e.preventDefault();
+        var textEl = ReactDOM.findDOMNode(this.refs.text);
 
-    onBtnClickHandler: function() {
-        alert(this.state.myValue);
+        var author = ReactDOM.findDOMNode(this.refs.author).value;
+        var text = textEl.value;
+
+        var item = [{
+            author: author,
+            text: text,
+            bigText: '...'
+        }];
+
+        window.ee.emit('News.add', item);
+
+        textEl.value = '';
+        this.setState({textIsEmpty: true});
     },
-
+    onCheckRuleClick: function(e) {
+        this.setState({agreeNotChecked: !this.state.agreeNotChecked});
+    },
+    onFieldChange: function(fieldName, e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({[''+fieldName]:false})
+        } else {
+            this.setState({[''+fieldName]:true})
+        }
+    },
     render: function() {
+        var agreeNotChecked = this.state.agreeNotChecked,
+            authorIsEmpty = this.state.authorIsEmpty,
+            textIsEmpty = this.state.textIsEmpty;
         return (
-            <div>
-                <input className='test-input'
-                value={this.state.myValue}
-                onChange={this.onChangeHandler}
-                placeholder='введите значение'
+            <form className='add cf'>
+                <input
+                    type='text'
+                    className='add__author'
+                    onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+                    placeholder='Ваше имя'
+                    ref='author'
                 />
-                <button onClick={this.onBtnClickHandler}>Показать alert</button>
-            </div>
+                <textarea
+                    className='add__text'
+                    onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+                    placeholder='Текст новости'
+                    ref='text'
+                ></textarea>
+                <label className='add__checkrule'>
+                    <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
+                </label>
+
+                <button
+                    className='add__btn'
+                    onClick={this.onBtnClickHandler}
+                    ref='alert_button'
+                    disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
+                >
+                    Опубликовать новость
+                </button>
+            </form>
         );
     }
 });
 
 var App = React.createClass({
+    getInitialState: function() {
+        return {
+            news: my_news
+        };
+    },
+    componentDidMount: function() {
+        var self = this;
+        window.ee.addListener('News.add', function(item) {
+            var nextNews = item.concat(self.state.news);
+            self.setState({news: nextNews});
+        });
+    },
+    componentWillUnmount: function() {
+        window.ee.removeListener('News.add');
+    },
     render: function() {
+        console.log('render');
         return (
             <div className='app'>
+                <Add />
                 <h3>Новости</h3>
-                <TestInput /> {/* добавили вывод компонента */}
-                <News data={my_news} />
+                <News data={this.state.news} />
             </div>
         );
     }
